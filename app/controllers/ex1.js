fc5e12417d12ddf7aa6ab2e2ba998f41a91e6331
Taino Corentin <cprentintaino2b@gmail.com>
2022-03-11 13:54:28 +0100

    TD 1 compris a 95 %, manque juste quelques connaissance en HTML

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,15 +1,33 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-import { action, get } from '@ember/object';
+import { action } from '@ember/object';
+
 
 export default class Ex1Controller extends Controller {
+
   @tracked leText = 'Il était une fois il y a fort longtemps';
+  noteModifier = 'Il était une fois il y a fort longtemps';
+
+  get classTailleText(){
+    if(this.leText.length <= 30)
+      return 'bg-success';
+    if(this.leText.length <= 70)
+      return 'bg-warning';
+    else
+      return 'bg-danger';
+  }
 
   @action effacerLeText() {
     this.leText = '';
   }
 
-  @get nbCharRestant() {
+  get nbCharRestant() {
     return 100 - this.leText.length;
+  }
+
+  get estSave(){
+    if(this.leText == this.noteModifier)
+      return false;
+    return true;
   }
 }
